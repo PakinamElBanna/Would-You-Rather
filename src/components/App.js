b524efdf4471d10569eb31412540ce24a7ad945b
Pakinam El Banna <pakinam_banna@hotmail.com>
2018-09-21 18:56:54 +0200

    Initialize basic layout

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route} from 'react-router-dom'
+import './App.css'
+import Icon from '@material-ui/core/Icon'
+import logo from '../logo.svg'
+import { TiHeartFullOutline } from 'react-icons/ti/index'
+
+import Login from './Login'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <img className='logo' src={logo} />
+        <div className="App-container">
+          <Router>
+            <Route path='/' exact component={Login} />
+          </Router>
+        </div>
+        <div className="App-footer">
+          <p>Made with <TiHeartFullOutline className="footer-icon"/> by: Pakinam EL Banna</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
