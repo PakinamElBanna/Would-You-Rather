682b8cc3941a5137ae4b28c36b035359f599ac01
Pakinam El Banna <pakinam_banna@hotmail.com>
2018-09-22 04:36:02 +0200

    remove unecessary helper function

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -7,23 +7,3 @@ export function formatQuestion (question) {
     author
   }
 }
-
-export function formatTweet (tweet, author, authedUser, parentTweet) {
-  const { id, likes, replies, text, timestamp } = tweet
-  const { name, avatarURL } = author
-
-  return {
-    name,
-    id,
-    timestamp,
-    text,
-    avatar: avatarURL,
-    likes: likes.length,
-    replies: replies.length,
-    hasLiked: likes.includes(authedUser),
-    parent: !parentTweet ? null : {
-      author: parentTweet.author,
-      id: parentTweet.id,
-    }
-  }
-}
